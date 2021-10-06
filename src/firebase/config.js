8f3ee989a14673d07cc8733031ab45a8d1c2740f
danielalonso77 <54615444+danielalonso77@users.noreply.github.com>
2021-10-05 23:11:43 -0500

    n
    n

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// AGREGA TUS CREDENCIALES AQUI
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCylBY4Sgpm8WSctEfrbfn6xi7EZWmmuys",
+    authDomain: "restaurante-cdb96.firebaseapp.com",
+    projectId: "restaurante-cdb96",
+    storageBucket: "restaurante-cdb96.appspot.com",
+    messagingSenderId: "504454623489",
+    appId: "1:504454623489:web:0d2e9aa8b9deaadd9bab00",
+    measurementId: "G-V7T03SG9W5"
+  };
+
+export default firebaseConfig;
